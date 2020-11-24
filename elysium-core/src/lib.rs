2734e2fa4717d6c9f7575518bb00f2a917375cf7
Jan Tebernum <jan.tebernum@gmail.com>
2020-11-24 20:04:44 +0100

    Moved initialization into private functions
    Structs shouldn't be in an uninitialized state in Rust. We need to
    initialize the whole struct in one go. The code to do so was moved into
    individual functions that are called in the new functions which will
    return the initialized struct.

--- elysium-core/src/lib.rs
+++ elysium-core/src/lib.rs
@@ -6,18 +6,20 @@ use std::thread;
 
 use log::{debug, error, info, warn};
 
-use vulkano::command_buffer::pool::CommandPool;
-use vulkano::command_buffer::pool::CommandPoolBuilderAlloc;
-use vulkano::command_buffer::AutoCommandBufferBuilder;
-use vulkano::device::{Device, DeviceExtensions, Queue};
-use vulkano::instance::{Instance, PhysicalDevice};
-use vulkano::swapchain::Surface;
 use vulkano::{
-    command_buffer::{pool::standard::StandardCommandPoolBuilder, AutoCommandBuffer},
-    image::ImageUsage,
-    image::SwapchainImage,
-    swapchain::FullscreenExclusive,
-    swapchain::{ColorSpace, PresentMode, SurfaceTransform, Swapchain},
+    command_buffer::{
+        pool::{standard::StandardCommandPoolBuilder, CommandPool, CommandPoolBuilderAlloc},
+        AutoCommandBuffer, AutoCommandBufferBuilder,
+    },
+    device::{Device, DeviceExtensions, Queue},
+    format::Format,
+    framebuffer::RenderPassAbstract,
+    image::{ImageUsage, SwapchainImage},
+    instance::{Instance, PhysicalDevice},
+    swapchain::{
+        ColorSpace, FullscreenExclusive, PresentMode, Surface, SurfaceCreationError,
+        SurfaceTransform, Swapchain, SwapchainCreationError,
+    },
 };
 
 use vulkano_win::VkSurfaceBuild;
@@ -26,9 +28,9 @@ use winit::window::{Window, WindowBuilder};
 
 #[allow(dead_code)]
 pub struct Elysium {
-    vk_instance: Arc<Instance>,
-    vk_physical_device_index: usize,
-    vk_device: Arc<Device>,
+    instance: Arc<Instance>,
+    physical_device_index: usize,
+    device: Arc<Device>,
 
     graphics_queue: Arc<Queue>,
 
@@ -37,6 +39,7 @@ pub struct Elysium {
     dimensions: [u32; 2],
     swapchain: Arc<Swapchain<Window>>,
     swapchain_images: Vec<Arc<SwapchainImage<Window>>>,
+    renderpass: Arc<dyn RenderPassAbstract + Send + Sync>,
 
     surface: Arc<Surface<Window>>,
     event_loop: EventLoop<()>,
@@ -48,117 +51,163 @@ impl Elysium {
     }
 
     pub fn new() -> Self {
-        let vk_instance = {
-            let required_extensions = vulkano_win::required_extensions();
-            Instance::new(None, &required_extensions, None)
-                .expect("Couldn't create the Vulkan instance")
-        };
+        let instance = Self::create_instance();
 
-        let vk_physical_device_index = {
-            let physical = PhysicalDevice::enumerate(&vk_instance)
-                .next()
-                .expect("No device chosen");
-            info!("Using physical device: {:?}", physical.name());
-            physical.index()
-        };
-
-        info!("Physical device index: {}", vk_physical_device_index);
+        let physical_device_index = Self::create_physical(instance.clone());
+        let physical = PhysicalDevice::from_index(&instance, physical_device_index).unwrap();
+        info!("Physical device index: {}", physical_device_index);
 
         let event_loop = EventLoop::new();
         info!("Created new EventLoop");
 
         let surface = WindowBuilder::new()
-            .build_vk_surface(&event_loop, vk_instance.clone())
+            .build_vk_surface(&event_loop, instance.clone())
             .unwrap();
 
         let dimensions = surface.window().inner_size().into();
 
         info!("Surface build with dimensions: {:?}", dimensions);
 
-        let physical = PhysicalDevice::from_index(&vk_instance, vk_physical_device_index).unwrap();
+        let (device, graphics_queue) = Self::create_device_and_queue(physical, surface.clone());
 
-        let (device, mut queues) = {
-            let device_ext = DeviceExtensions {
-                khr_swapchain: true,
-                ..DeviceExtensions::none()
-            };
+        let (swapchain, swapchain_images) = Self::create_swapchain(
+            device.clone(),
+            physical,
+            surface.clone(),
+            dimensions,
+            graphics_queue.clone(),
+        )
+        .unwrap();
 
-            info!(
-                "GPU has {} queue family(ies)",
-                physical.queue_families().len()
-            );
-            let queue_family = physical
-                .queue_families()
-                .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
-                .unwrap();
-
-            Device::new(
-                physical,
-                physical.supported_features(),
-                &device_ext,
-                [(queue_family, 0.5)].iter().cloned(),
-            )
-            .unwrap()
-        };
-
-        info!("{} graphic queue(s) available", queues.len());
-
-        let graphics_queue = queues.next().unwrap();
-
-        let (mut swapchain, swapchain_images) = {
-            let caps = surface.capabilities(physical).unwrap();
-            let format = caps.supported_formats[0].0;
-            let alpha = caps.supported_composite_alpha.iter().next().unwrap();
-
-            Swapchain::new(
-                device.clone(),
-                surface.clone(),
-                caps.min_image_count,
-                format,
-                dimensions,
-                1,
-                ImageUsage::color_attachment(),
-                &graphics_queue,
-                SurfaceTransform::Identity,
-                alpha,
-                PresentMode::Fifo,
-                FullscreenExclusive::Default,
-                true,
-                ColorSpace::SrgbNonLinear,
-            )
-            .unwrap()
-        };
-
-        let vk_device = device;
+        let renderpass = Self::create_renderpass(device.clone(), swapchain.clone());
 
         Self {
-            vk_instance,
-            vk_physical_device_index,
-            vk_device,
+            instance,
+            physical_device_index,
+            device,
             graphics_queue,
             command_buffer: vec![],
             dimensions,
             swapchain,
             swapchain_images,
+            renderpass,
             surface,
             event_loop,
         }
+    }
+
+    fn create_instance() -> Arc<Instance> {
+        let required_extensions = vulkano_win::required_extensions();
+        Instance::new(None, &required_extensions, None)
+            .expect("Couldn't create the Vulkan instance")
+    }
+
+    fn create_physical(instance: Arc<Instance>) -> usize {
+        let physical = PhysicalDevice::enumerate(&instance)
+            .next()
+            .expect("No device chosen");
+        info!("Using physical device: {:?}", physical.name());
+        physical.index()
+    }
+
+    fn create_device_and_queue(
+        physical: PhysicalDevice,
+        surface: Arc<Surface<Window>>,
+    ) -> (Arc<Device>, Arc<Queue>) {
+        let device_ext = DeviceExtensions {
+            khr_swapchain: true,
+            ..DeviceExtensions::none()
+        };
+
+        info!(
+            "GPU has {} queue family(ies)",
+            physical.queue_families().len()
+        );
+        let queue_family = physical
+            .queue_families()
+            .find(|&q| q.supports_graphics() && surface.is_supported(q).unwrap_or(false))
+            .unwrap();
+
+        let (device, mut queues) = Device::new(
+            physical,
+            physical.supported_features(),
+            &device_ext,
+            [(queue_family, 0.5)].iter().cloned(),
+        )
+        .unwrap();
+
+        (device, queues.next().unwrap())
+    }
+
+    fn create_swapchain(
+        device: Arc<Device>,
+        physical: PhysicalDevice,
+        surface: Arc<Surface<Window>>,
+        dimensions: [u32; 2],
+        graphics_queue: Arc<Queue>,
+    ) -> Result<(Arc<Swapchain<Window>>, Vec<Arc<SwapchainImage<Window>>>), SwapchainCreationError>
+    {
+        let caps = surface.capabilities(physical).unwrap();
+        let format = caps.supported_formats[0].0;
+        let alpha = caps.supported_composite_alpha.iter().next().unwrap();
+
+        Swapchain::new(
+            device.clone(),
+            surface.clone(),
+            caps.min_image_count,
+            format,
+            dimensions,
+            1,
+            ImageUsage::color_attachment(),
+            &graphics_queue,
+            SurfaceTransform::Identity,
+            alpha,
+            PresentMode::Fifo,
+            FullscreenExclusive::Default,
+            true,
+            ColorSpace::SrgbNonLinear,
+        )
+    }
+
+    fn create_renderpass(
+        device: Arc<Device>,
+        swapchain: Arc<Swapchain<Window>>,
+    ) -> Arc<dyn RenderPassAbstract + Send + Sync> {
+        Arc::new(
+            vulkano::single_pass_renderpass!(device.clone(),
+                attachments: {
+                color: {
+                    load: Clear,
+                    store: Store,
+                    format: swapchain.format(),
+                    samples: 1,
+                },
+                depth: {
+                    load: Clear,
+                    store: DontCare,
+                    format: Format::D16Unorm,
+                    samples: 1,
+                }
+                },
+                pass: {
+                    color: [color],
+                    depth_stencil: {depth}
+                }
+            )
+            .unwrap(),
+        )
     }
 
     //fn create_command_buffers(&mut self) {
     //    let queue_family = self.graphics_queue.family();
     //    self.command_buffer = self.swapchain_images.iter()
     //        .map(|framebuffer| {
-    //            Arc::new(AutoCommandBufferBuilder::primary_simultaneous_use(self.vk_device.clone(), queue_family)
+    //            Arc::new(AutoCommandBufferBuilder::primary_simultaneous_use(self.device.clone(), queue_family)
     //                .unwrap()
     //                .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])
     //                .unwrap()
     //                .draw_indexed(pipeline, dynamic, vertex_buffer, index_buffer, sets, constants)
     //        })
-    //}
-
-    //pub fn init(&mut self) {
-    //    self.event_loop = EventLoop::new();
     //}
 }
 
